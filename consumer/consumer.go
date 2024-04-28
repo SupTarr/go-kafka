b072be3a382960f527bf0667699c96c86178b6b3
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2024-04-28 08:53:03 +0700

    feat: add sarama consumer template

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,12 +1,36 @@
-package main
+package consumer
 
 import (
+	"context"
+	"errors"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/IBM/sarama"
 )
 
-func toggleConsumptionFlow(client sarama.ConsumerGroup, isPaused *bool) {
+type Consumer struct {
+	config  *sarama.Config
+	ready   chan bool
+	brokers []string
+	topics  []string
+	group   string
+}
+
+func NewConsumer(brokers []string, topics []string, group string) *Consumer {
+	return &Consumer{
+		config:  sarama.NewConfig(),
+		ready:   make(chan bool),
+		brokers: brokers,
+		topics:  topics,
+		group:   group,
+	}
+}
+
+func (consumer *Consumer) toggleConsumptionFlow(client sarama.ConsumerGroup, isPaused *bool) {
 	if *isPaused {
 		client.ResumeAll()
 		log.Println("Resuming consumption")
@@ -16,10 +40,6 @@ func toggleConsumptionFlow(client sarama.ConsumerGroup, isPaused *bool) {
 	}
 
 	*isPaused = !*isPaused
-}
-
-type Consumer struct {
-	ready chan bool
 }
 
 func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
@@ -44,5 +64,66 @@ func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, clai
 		case <-session.Context().Done():
 			return nil
 		}
+	}
+}
+
+func (consumer *Consumer) Consume() {
+	keepRunning := true
+	log.Println("Starting a new Sarama consumer")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	client, err := sarama.NewConsumerGroup(consumer.brokers, consumer.group, consumer.config)
+	if err != nil {
+		log.Panicf("Error creating consumer group client: %v", err)
+	}
+
+	consumptionIsPaused := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			if err := client.Consume(ctx, consumer.topics, consumer); err != nil {
+				if errors.Is(err, sarama.ErrClosedConsumerGroup) {
+					return
+				}
+				log.Panicf("Error from consumer: %v", err)
+			}
+
+			if ctx.Err() != nil {
+				return
+			}
+			consumer.ready = make(chan bool)
+		}
+	}()
+
+	<-consumer.ready
+
+	log.Println("Sarama consumer up and running!...")
+
+	sigusr1 := make(chan os.Signal, 1)
+	signal.Notify(sigusr1, syscall.SIGUSR1)
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+
+	for keepRunning {
+		select {
+		case <-ctx.Done():
+			log.Println("Terminating consumer: context cancelled")
+			keepRunning = false
+		case <-sigterm:
+			log.Println("Terminating consumer: via signal")
+			keepRunning = false
+		case <-sigusr1:
+			consumer.toggleConsumptionFlow(client, &consumptionIsPaused)
+		}
+	}
+
+	cancel()
+	wg.Wait()
+	if err = client.Close(); err != nil {
+		log.Panicf("Error closing client: %v", err)
 	}
 }
